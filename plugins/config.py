4987f0f05448a8205d54c52e9bd1fce9bdd89508
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2025-02-06 20:51:53 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -44,7 +44,7 @@ class Config(object):
     ADL_BOT_RQ = {}
 
     # Set False off else True
-    TRUE_OR_FALSE = os.environ.get("TRUE_OR_FALSE", "").lower() == "true"
+    TRUE_OR_FALSE = os.environ.get("TRUE_OR_FALSE", "").lower() == "False"
 
     # Shortlink settings
     SHORT_DOMAIN = environ.get("SHORT_DOMAIN", "")
